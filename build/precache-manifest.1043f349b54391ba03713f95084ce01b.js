320d35ffac934f9d1964dbb41da30a6202326672
JonSchaeffer <jschaeffer97@gmail.com>
2019-04-21 23:25:46 -0400

    A todo list using ReactJS

---
+++ build/precache-manifest.1043f349b54391ba03713f95084ce01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56dab1f33f1ae759a7ba",
+    "url": "/static/js/main.177dda28.chunk.js"
+  },
+  {
+    "revision": "254a14c09bfef11176c1",
+    "url": "/static/js/2.ccc89068.chunk.js"
+  },
+  {
+    "revision": "56dab1f33f1ae759a7ba",
+    "url": "/static/css/main.c810940b.chunk.css"
+  },
+  {
+    "revision": "590b08b16bc72d8832f9889e6c43e4f2",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is the TodoList app v1.0.0. This is used to study and become familiar with ReactJS</p>
+    </React.Fragment>
+  )
+}
